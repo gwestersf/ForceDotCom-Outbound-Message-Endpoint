33c3464fe9ae8440c05c28ca5e4d5a0a63c7add2
Gregory Wester <gwester@salesforce.com>
2012-02-25 16:38:40 -0800

    had to set depenencies in cp

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java -cp target/classes:target/crm-bridge.jar com.heroku.crm.HerokuApp
+web:    java -cp target/classes:target/dependency/* com.heroku.crm.HerokuApp
--- src/main/java/com/heroku/crm/HerokuApp.java
+++ src/main/java/com/heroku/crm/HerokuApp.java
@@ -7,14 +7,14 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class HerokuApp {
 	 public static void main(String[] args) throws Exception {
-	  String webPort = System.getenv("PORT");
-	  String webappDirLocation = "src/main/webapp/";
-	 
-	  if(webPort == null || webPort.isEmpty()) {
-	        webPort = "8443";
-	  }
-	  
-	  Server server = new Server(Integer.valueOf(webPort));
+	     String webPort = System.getenv("PORT");
+	     String webappDirLocation = "src/main/webapp/";
+	     
+	     if(webPort == null || webPort.isEmpty()) {
+	           webPort = "8443";
+	     }
+	      
+		 Server server = new Server(Integer.valueOf(webPort));
 	     WebAppContext root = new WebAppContext();
 	     root.setContextPath("/");
 	     root.setDescriptor(webappDirLocation+"/WEB-INF/web.xml");
